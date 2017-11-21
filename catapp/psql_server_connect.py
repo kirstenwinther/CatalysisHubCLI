5ae09f4deaba53caf42240b4df4ecef6b0a105b5
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-20 21:10:28 -0800

    Turned last sh script into python script.

--- catapp/psql_server_connect.py
+++ catapp/psql_server_connect.py
@@ -1,1 +1,5 @@
+import subprocess
+
+subprocess.check_output('
 psql --host=catappdatabase.cjlis1fysyzx.us-west-1.rds.amazonaws.com --port=5432 --username=catappuser --dbname=catappdatabase --password
+'.split())
