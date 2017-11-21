30f22ba4af56f52929f7de152ccfa46e9e72b389
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-20 20:20:33 -0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(  name='catapp',
-        version='0.0.1',
-        packages=['catapp'],
-        install_requires=[
-            'Click',
-            ],
-        entry_points='''
+setup(name='catapp',
+      version='0.0.1',
+      packages=['catapp'],
+      install_requires=[
+          'Click',
+          'six',
+      ],
+      entry_points='''
             [console_scripts]
             catapp=catapp:cli
         ''')
