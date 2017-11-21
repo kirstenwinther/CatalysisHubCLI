cf4c527ad449b2acb5406af2a157d6d36966e274
Max Hoffmann <mjhoffmann@gmail.com>
2017-11-20 20:32:35 -0800

    Put main code in main() function so that script can be used from command line

--- catapp/convert_traj.py
+++ catapp/convert_traj.py
@@ -16,13 +16,17 @@ except:  # SUNCAT
 path.append(catbase)
 from ase_tools import check_traj
 
+
+def main(base):
+    for roots, dirs, files in os.walk(base):
+        traj_files = [f for f in files if f.endswith('traj')]
+        for f in traj_files:
+            check_traj('{}/{}'.format(roots, f))
+
 try:
     base = argv[1]
 except:
     base = '.'
 
-for roots, dirs, files in os.walk(base):
-    traj_files = [f for f in files if f.endswith('traj')]
-    for f in traj_files:
-        check_traj('{}/{}'.format(roots, f))
-
+if __name__ == '__main__':
+    main(base)
