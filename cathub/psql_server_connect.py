6e09c4b8d8da7985fcec750c2c4c0caf490561cc
Kirsten Winther <winther@stanford.edu>
2018-03-27 15:04:52 -0700

    new server

--- cathub/psql_server_connect.py
+++ cathub/psql_server_connect.py
@@ -5,3 +5,4 @@ user = argv[1]
 
 os.system('psql --host=catappdatabase.cjlis1fysyzx.us-west-1.rds.amazonaws.com --port=5432 --username={} --dbname=catappdatabase --password'.format(user))
 
+#os.system('psql --host=catalysishub.c8gwuc8jwb7l.us-west-2.rds.amazonaws.com --port=5432 --username={} --dbname=catalysishub --password'.format(user))
